8567462dd887e86ae26b0a68681e41828ee64f45
Di-Doan <92972404+Di-Doan@users.noreply.github.com>
2023-12-09 21:53:37 +0700

    add forgot and reset password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LoginForm from "./components/Login/LoginForm";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import ResetPassword from "./components/ForgotPassword/ResetPassword";
+
 import UserProfile from "./components/UserProfile/UserProfile";
 import Design from "./components/Dashboard/Design";
 
@@ -10,7 +13,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* // password and login  */}
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/dashboard" element={<Design />} />
       </Routes>
