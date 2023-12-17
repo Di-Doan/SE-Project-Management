f51dca7b0faa7088823b6838a7084db40b10eb7a
Phuong <s3885751@rmit.edu.vn>
2023-12-17 23:53:12 +0700

    "update DirectChatPage and DirectChatSidebar"

--- frontend/src/components/Direct Chat/DirectChatSidebar.js
+++ frontend/src/components/Direct Chat/DirectChatSidebar.js
@@ -3,11 +3,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from './DirectChatSidebar.css'
+import ava1 from "../../assets/avatar2.jpg";
 
 const DirectChatSidebar = () => {
   const [chats, setChats] = useState([
-    { id: 1, name: 'John Doe', lastMessage: 'Hello there!', unreadCount: 2 },
-    { id: 2, name: 'Alice Smith', lastMessage: 'How are you?', unreadCount: 0 },
+    { id: 1, name: 'John Doe', lastMessage: 'Hello there!', unreadCount: 0 },
+    { id: 2, name: 'Alice Smith', lastMessage: 'How are you?', unreadCount: 1 },
     // Add more chat objects as needed
   ]);
 
@@ -16,7 +17,7 @@ const DirectChatSidebar = () => {
       <div className="chat-list">
         {chats.map((chat) => (
           <Link to={`/chat/${chat.id}`} key={chat.id} className="chat-item">
-            <div className="chat-avatar"> {/* Add avatar here */}</div>
+            <div className="chat-avatar"><img src={ava1} /></div>
             <div className="chat-details">
               <div className="chat-name">{chat.name}</div>
               <div className="last-message">{chat.lastMessage}</div>
