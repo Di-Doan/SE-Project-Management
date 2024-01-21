8e87b678260687505d80975ad9da15e8010af690
Di-Doan <92972404+Di-Doan@users.noreply.github.com>
2024-01-21 15:31:47 +0700

    add course modal to side bar

--- frontend/src/components/Modal/CourseModal.js
+++ frontend/src/components/Modal/CourseModal.js
@@ -14,8 +14,11 @@ function CourseModal({ setModal, modal, course }) {
         <Modal.Title>Current Course</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {course ?course.filter(filtered=> filtered.status == 1).map(item=> (<div>
-          {item.name}
+        {course ?course.filter(filtered=> filtered.status == 1).map(item=> (<div className="modal-box"> 
+          <div className="modal-course">
+            {item.name}
+          </div>
+          <div>{item.code}</div>
         </div>)) : null}
       </Modal.Body>
     </Modal>
