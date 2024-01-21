44f73b6511aeb1e0038edf1fbaedb63d67b25f7c
Phuong Hoang <s3885751@rmit.edu.vn>
2024-01-21 17:24:30 +0700

    update right side bar

--- frontend/src/components/CoursesChat/RightSidebar.js
+++ frontend/src/components/CoursesChat/RightSidebar.js
@@ -1,18 +1,34 @@
-// RightSidebar.js
-import React from 'react';
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 import './RightSidebar.css';
+import { useParams } from 'react-router-dom'; // Import useParams
 
-const RightSidebar = ({ users }) => {
+function RightSidebar() {
+  const [people, setPeople] = useState([]);
+  const { course_id } = useParams(); // Get the course_id from useParams
+
+  const getPeople = async () => {
+    try {
+      const response = await axiosInstance.get(
+        `/courses/${course_id}/students`
+      );
+      setPeople(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPeople();
+    getData();
+  }, []);
+
   return (
     <div className="right-sidebar">
       <h2>Users</h2>
       <ul>
-        {Array.isArray(users) && users.length > 0 ? (users.map((user) => (
+        {Array.isArray(people) && people.length > 0 ? (people.map((user) => (
           <li key={user.id}>
-            <div className="user-avatar">
-              <img src={user.avatar} alt={`Avatar of ${user.name}`} />
-            </div>
             <div className="user-name">{user.name}</div>
           </li>
         ))
@@ -22,6 +38,6 @@ const RightSidebar = ({ users }) => {
       </ul>
     </div>
   );
-};
+}
 
 export default RightSidebar;
